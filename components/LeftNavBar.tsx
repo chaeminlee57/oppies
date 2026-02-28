4390993281c408093cd5b6f964c19d63b1d8fe9c
Chaemin Lee <chaeminlee57@gmail.com>
2026-02-28 17:43:02 -0600

    replaced left nav bar with emojis

--- components/LeftNavBar.tsx
+++ components/LeftNavBar.tsx
@@ -1,14 +1,15 @@
 import styles from './LeftNavBar.module.scss'
 import { playfairDisplay } from '../app/fonts';
+import { House, Newspaper, PenLine, User, Mail } from 'lucide-react';
 
 export default function LeftNavBar() {
   return(
     <nav className={`${styles.navWrapper} ${playfairDisplay.variable}`}>
-      <a href="#" className={styles.navLink}><h2 className={styles.navItem}>Home</h2></a>
-      <a href="#" className={styles.navLink}><h2 className={styles.navItem}>Feed</h2></a>
-      <a href="#" className={styles.navLink}><h2 className={styles.navItem}>Write</h2></a>
-      <a href="#" className={styles.navLink}><h2 className={styles.navItem}>Profile</h2></a>
-      <a href="#" className={styles.navLink}><h2 className={styles.navItem}>Contact</h2></a>
+      <a href="#" className={styles.navLink}><House className={styles.navItem} /></a>
+      <a href="#" className={styles.navLink}><Newspaper className={styles.navItem} /></a>
+      <a href="#" className={styles.navLink}><PenLine className={styles.navItem} /></a>
+      <a href="#" className={styles.navLink}><User className={styles.navItem} /></a>
+      <a href="#" className={styles.navLink}><Mail className={styles.navItem} /></a>
     </nav>
   )
 }
